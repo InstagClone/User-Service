a816ca145ad7219c61d4fbc04eb42d2fa32dff75
Songren Zhao <songrenzhao@gmail.com>
2021-02-03 23:41:20 -0500

    minor changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv/config';
 
 export default {
   port: process.env.PORT,
-  jwtKey: process.env.JWT_KEY,
   pg_connection_string: process.env.PG_CONNECTION_STRING
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Logger from './utils/logger';
 import type { Application } from 'express';
 
 const app: Application = createApp();
-
+console.log(config.port);
 app.listen(config.port || 3000, (): void => {
   Logger.info(`Server running on port ${config.port}`);
 });
